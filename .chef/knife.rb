136b6d8db5b63484c39f8fabf906fe8d5ef28076
kuma <kuma@morikuma.org>
2013-09-13 18:38:14 +0900

    dmgとかhomebrewとか追記

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,7 +1,8 @@
-cookbook_path ["cookbooks", "site-cookbooks"]
-node_path     "nodes"
-role_path     "roles"
-data_bag_path "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
-
-knife[:berkshelf_path] = "cookbooks"
+log_level                :info
+log_location             STDOUT
+node_name                'kuma'
+client_key               '/Users/kuma/project/chef-for-mac/.chef/kuma.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://k.local:443'
+syntax_check_cache_path  '/Users/kuma/project/chef-for-mac/.chef/syntax_check_cache'
